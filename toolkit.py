c569f630792839a274568c9e00edcec339d56286
z0l <z0l@z0l>
2015-06-08 12:53:03 +0200

    add method to check overlap of rectangles

--- toolkit.py
+++ toolkit.py
@@ -1,5 +1,5 @@
 import os,sys
-
+import numpy as np
 def scanfor(indir, obj_ext):
     result = []
     for rdir, pdir, names in os.walk(indir):
@@ -13,3 +13,17 @@ def scanfor(indir, obj_ext):
                    result.append([sname,fname])
     return result
 
+def inter2union(r1,r2):
+   ii = [0,0,0,0]
+   ii[0] = np.maximum(r1[0], r2[0])
+   ii[1] = np.maximum(r1[1], r2[1])
+   ii[2] = np.minimum(r1[2], r2[2])
+   ii[3] = np.minimum(r1[3], r2[3])
+
+   iw = ii[2] - ii[0]
+   ih = ii[3] - ii[1]
+   ovr = 0
+   if iw > 0 and ih > 0:
+       total = (r1[2] - r1[0]) * (r1[3] - r1[1]) + (r2[2] - r2[0]) * (r2[3] - r2[1])
+       ovr = iw * ih * 1.0/ (total - iw * ih)
+   return ovr
